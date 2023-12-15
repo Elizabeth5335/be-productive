d442594687ae76da3540cf3b20b40f1ed15871f5
Elizabeth5335 <51746623+Elizabeth5335@users.noreply.github.com>
2023-12-15 23:47:31 +0200

    added timer

--- src/components/Pomodoro.js
+++ src/components/Pomodoro.js
@@ -1,9 +1,26 @@
+import Timer from "./Timer";
+import React from "react";
+
 export default function Pomodoro() {
+
+  const [timerState, setTimerState] = React.useState(false);
+  const [time, setTime] = React.useState(40);
+
+
+  function startTimer() {
+    setTimerState(prev => !prev);
+  }
 
   return (
     <div className="pomodoro">
-        <h1>Pomodoro</h1>
+      <h1>Pomodoro</h1>
+      <div style={{display: 'flex', justifyContent: 'space-evenly'}}>
+        <button onClick={()=>setTime(40)}>Pomodoro</button>
+        <button onClick={()=>setTime(10)}>Short break</button>
+        <button onClick={()=>setTime(15)}>Long break</button>
+      </div>
+      <Timer timerState={timerState} time={time} />
+      <button onClick={startTimer}>Start</button>
     </div>
   );
 }
-
---
+++ src/components/Timer.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState, useRef } from "react";
+
+export default function Timer(props) {
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+  const intervalRef = useRef(null);
+
+  const deadline = new Date(new Date().getTime() + props.time * 60000);
+
+  const startTimer = () => {
+    intervalRef.current = setInterval(() => getTime(deadline), 1000);
+  };
+
+  const pauseTimer = () => {
+    clearInterval(intervalRef.current);
+  };
+
+  useEffect(() => {
+    if (!props.timerState) {
+      pauseTimer();
+    } else {
+      startTimer();
+    }
+
+    return () => clearInterval(intervalRef.current);
+  }, [props.timerState]);
+
+  const getTime = () => {
+    const time = deadline.getTime() - Date.now();
+    setMinutes(Math.floor(time / 1000 / 60));
+    setSeconds(Math.floor((time / 1000) % 60));
+  };
+
+  return (
+    <div className="timer">
+      <div className="timer" style={{ display: 'flex', justifyContent: 'center' }} role="timer">
+        <div id="minute">{minutes < 10 ? "0" + minutes : minutes}</div>:
+        <div id="second">{seconds < 10 ? "0" + seconds : seconds}</div>
+      </div>
+    </div>
+  );
+}
